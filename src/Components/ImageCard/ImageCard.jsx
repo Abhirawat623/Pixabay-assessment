66362f811ac19dd0f5dc0ed1f69e4df381edf9a5
Abhishek Rawat <131130116+Abhirawat623@users.noreply.github.com>
2024-01-20 01:10:14 +0530

    fetched images as searched result and improved css

--- src/Components/ImageCard/ImageCard.jsx
+++ src/Components/ImageCard/ImageCard.jsx
@@ -1,17 +1,14 @@
 import './ImageCard.css';
 
-export const ImageCard=()=>{
+export const ImageCard=({items})=>{
+
+    const {largeImageURL}= items;
     return(
         <div className='image-card-container d-flex direction-column align-center no-border'>
             <div className='image-container'>
-                <img className='card-image border-radius-m' src='../wallpapers/wallpaper1.jpg'/>
-                <div className='card-image-tag-container d-flex direction-row space-between'>
-                    <span className='card-image-tag'>yellow</span>
-                    <span className='card-image-tag'>yellow</span>  
-                    <span className='card-image-tag'>yellow</span>
-                    
+                <img className='card-image border-radius-m' src={largeImageURL}/>
+              
                 </div>
             </div>
-        </div>
     )
 }
--- src/Pages/Searchpage/Searchpage.jsx
+++ src/Pages/Searchpage/Searchpage.jsx
@@ -1,28 +1,13 @@
 import './Searchpage.css';
 import {wallpaper} from '../../assets/wallpaper';
 import {Navbar,Searchbar,ResultContainer} from '../../Components/index';
-import { useEffect, useState } from 'react';
-import axios from 'axios';
+
 export const Searchpage=()=>{
         
     //for random wallpaper for searchpage
         const index= Math.floor(Math.random()*(wallpaper.length-1));
         const randomWallpaper=wallpaper[index].thumbnail;
-    //fetching searched images from api
-
-    const [searchedImages,setSearchedImages]= useState([]);
-    useEffect(()=>{
-        (async()=>{
-            try{
-                const{data}= await axios
-                .get(`https://pixabay.com/api/?key=41898847-0dd4f5c4dfd622666224d0c7d&q`);
-                setSearchedImages(data);
-            console.log(searchedImages)}
-            catch(error) {console.log(error)}
-        })
-        ()
-
-    },[])
+    
 
     return(
 <main className="display-wallpaper d-flex direction-column align-center gap-l"
