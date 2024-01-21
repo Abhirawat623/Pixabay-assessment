64d410adf2e0044f742bb2d84eab755aff0d9a00
Abhishek Rawat <131130116+Abhirawat623@users.noreply.github.com>
2024-01-21 10:33:18 +0530

    added login and logout logics based on access token

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,21 +1,28 @@
 import "./Navbar.css";
+import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
 import { useAuth } from "../../Context/index";
 export const Navbar = () => {
-  const { accessToken,authDispatch } = useAuth();
-  console.log({accessToken});
+  const { authDispatch } = useAuth();
+  const navigate = useNavigate();
   //for login modal
   const handleLoginModalClick = () => {
     authDispatch({
       type: "LOGIN_MODAL",
     });
   };
-  //for signup modal
+  //for token from local storage
+  const token = localStorage.getItem("token");
   //for signup modal open
   const handleSignupmodalopen = () => {
     authDispatch({
       type: "SIGNUP_MODAL",
     });
+  };
+  //for logout
+  const handleLogoutClick = () => {
+    navigate("/");
+    localStorage.removeItem("token");
   };
   return (
     <nav
@@ -26,36 +33,26 @@ export const Navbar = () => {
         <h3>Homepage</h3>
       </Link>
 
-{accessToken ?
-(<div className="d-flex direction-row align-center  gap-m">
-<Link to="/login" onClick={handleLoginModalClick}>
-  <h3 className="login-btn text-s no-border">
-    <span id='logout-icon'><span class="material-symbols-outlined cursor">
-logout
-</span></span>
-  </h3>
-</Link>
-<Link to="/signup" onClick={handleSignupmodalopen}>
-  <button
-    className="create-acc-btn primary-text no-borer border-radius-l text-s "
-  >
-    username
-  </button>
-</Link>
-</div>) :
-(<div className="d-flex direction-row align-center  gap-m">
-<Link to="/login" onClick={handleLoginModalClick}>
-  <h3 className="login-btn text-s no-border"> Login</h3>
-</Link>
-<Link to="/signup" onClick={handleSignupmodalopen}>
-  <button
-    className="create-acc-btn primary-text no-borer border-radius-l text-s "
-  >
-    Create Account
-  </button>
-</Link>
-</div>)}
-
+      {token ? (
+        <div className="d-flex direction-row align-center  gap-m">
+          <Link to="/signup" onClick={handleLogoutClick}>
+            <button className="create-acc-btn primary-text no-borer border-radius-l text-s cursor">
+              Logout
+            </button>
+          </Link>
+        </div>
+      ) : (
+        <div className="d-flex direction-row align-center  gap-m">
+          <Link to="/login" className="login-btn" onClick={handleLoginModalClick}>
+            <h3 className=" text-s no-border">Login</h3>
+          </Link>
+          <Link to="/signup" onClick={handleSignupmodalopen}>
+            <button className="create-acc-btn primary-text no-borer border-radius-l text-s ">
+              Create Account
+            </button>
+          </Link>
+        </div>
+      )}
     </nav>
   );
 };
